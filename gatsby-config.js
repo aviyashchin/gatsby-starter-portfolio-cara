13850ca1117e2282a7bfacfc20c22fdb0d706965
Avi Yashchin <avi.yashchin@gmail.com>
2019-11-25 16:48:11 -0500

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ require(`dotenv`).config({
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitleAlt: `Avi Yashchin Homepage`,
   },
   plugins: [
     {
@@ -20,9 +20,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `Avi Yashchin Homepage`,
+        short_name: `Avi`,
+        description: `Business + Data`,
         start_url: `/`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,
